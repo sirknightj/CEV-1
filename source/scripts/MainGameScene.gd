e542c87e960a0f9119a185a0942b5f0603ea7b06
Jeremy Gunawan <jeremyg1@cs.washington.edu>
2022-04-29 18:09:34 -0700

    Start making buildings affect the incomes

--- source/scripts/MainGameScene.gd
+++ source/scripts/MainGameScene.gd
@@ -5,10 +5,29 @@ var population # the current population
 var _turn_count_text # the text holder object that displays "Turn: 69"
 
 # note: we need income/expense to be seperate so that we can pass that into the graph
+var food : int          # the amount of food we have on turn
+var food_income : int   # the amount of food we gain on the next turn
+var food_expense : int  # the amount of food we will expend on the next turn, nonnegative
+
 var water : int          # the amount of water we have on turn
 var water_income : int   # the amount of water we gain on the next turn
-var water_expense : int  # the amount of water we will expend on the next turn
-# TODO: the other resources
+var water_expense : int  # the amount of water we will expend on the next turn, nonnegative
+
+var oxygen : int          # the amount of oxygen we have on turn
+var oxygen_income : int   # the amount of oxygen we gain on the next turn
+var oxygen_expense : int  # the amount of oxygen we will expend on the next turn, nonnegative
+
+var electricity : int          # the amount of electricity we have on turn
+var electricity_income : int   # the amount of electricity we gain on the next turn
+var electricity_expense : int  # the amount of electricity we will expend on the next turn, nonnegative
+
+var metal : int          # the amount of metal we have on turn
+var metal_income : int   # the amount of metal we gain on the next turn
+var metal_expense : int  # the amount of metal we will expend on the next turn, nonnegative
+
+var science : int          # the amount of science we have on turn
+var science_income : int   # the amount of science we gain on the next turn
+var science_expense : int  # the amount of science we will expend on the next turn, nonnegative
 
 # Grid Variables
 # A mutable grid_size x grid_size grid that buildings can be placed in.
@@ -20,7 +39,9 @@ var height : int  # the height per square
 var grid_size : int   # the number of squares the width of this grid is
 var buildings : Array # the buildings that we own on this grid
 
-var SideBar # the sidebar object
+var SideBar : Control # the sidebar object
+var Grid : Sprite # the grid object
+
 onready var Building = preload("res://Building.tscn")
 var buildings_json = "res://assets/data/buildings.json" # file path of buildings.json
 var buildings_dict : Dictionary = {} # building_name -> building
@@ -30,6 +51,7 @@ func _ready():
 	turn = 0
 	recalculate_grid_dims(15)
 	SideBar = get_node("Sidebar")
+	Grid = get_node("Grid")
 	load_buildings_json()
 	print('Got here!')
 
@@ -60,13 +82,16 @@ func on_next_turn():
 func place_building(_x: int, _y: int) -> void:
 	# TODO: load the buildings dynamically
 	var building = Building.instance()
-	# This is the farm shape
-	building.init_shape([[true,  true,  true,  true],
-						 [true,  true,  true,  false]], Color(0, 0, 0), width, 0, 0)
+	
+	# Testing out a farm
+	var building_stats = buildings_dict["Farm"]
+	var shape = building_stats.shape
+	building.init_shape(shape, Color(0, 0, 0), width, 0, 0)
 	# building.move_building(0, 6)
 	# building.rotate_building(90)
 	get_parent().add_child(building)
-	buildings.push_back(building)
+	buildings.push_back(building_stats)
+	recalculate_incomes()
 	# $Grid.visible = false
 
 """
@@ -91,8 +116,19 @@ func getGridSquareVector(_x : int, _y : int) -> Vector2:
 	# Calculate the total income we have
 """
 func recalculate_incomes() -> void:
+	var new_water_expense : int = 0
+	var new_water_income : int = 0
+
 	for building in buildings:
-		print(str(building.water_effects))
+		print(building["water_effect"])
+		if building["water_effect"] < 0:
+			new_water_expense -= building["water_effect"]
+		else:
+			new_water_income += building["water_effect"]
+
+	water_expense = new_water_expense
+	water_income = new_water_income
+	SideBar.update_displays()
 
 """
 	Returns the grid square the mouse is in
--- source/scripts/Sidebar.gd
+++ source/scripts/Sidebar.gd
@@ -7,12 +7,31 @@ extends Control
 # var b = "text"
 var Game # The node representing the game
 var Turn_Count_Text # The node holding the turn count text
+var Resource_Label_Text  # The text displayed next to each resource count
+var Resource_Amount_Text # The text displaying the number of each resource
+
+var show_water : bool
+var show_food : bool
+var show_electricity : bool
+var show_metal : bool
+var show_oxygen : bool
+var show_science : bool
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Game = get_parent()
 	Turn_Count_Text = get_node("TurnCount")
+	Resource_Label_Text = get_node("ResourceLabel")
+	Resource_Amount_Text = get_node("ResourceCounts")
+	
+	# TODO: progression
+	show_water = true
+	show_food = true
+	show_electricity = true
+	show_metal = true
+	show_oxygen = true
+	show_science = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -27,6 +46,40 @@ func _on_NextTurnButton_pressed() -> void:
 	update_turn_display()
 	Game.place_building(0, 0)
 	Game.on_next_turn()
-	
+
+"""
+	Updates the text displaying the turn count
+"""
 func update_turn_display() -> void:
 	Turn_Count_Text.text = "Month " + str(Game.turn)
+
+"""
+	Updates the text displaying the resource counts
+"""
+func update_displays() -> void:
+	var label_text : String = ""
+	var count_text = ""
+	
+	if show_water:
+		label_text += "Water:\n"
+		count_text += str(Game.water) + " (+" + str(Game.water_income) + ", -" + str(Game.water_expense) + ")\n"
+	if show_food:
+		label_text += "Food:\n"
+		count_text += str(Game.food) + " (+" + str(Game.food_income) + ", -" + str(Game.food_expense) + ")\n"
+	if show_oxygen:
+		label_text += "Water:\n"
+		count_text += str(Game.oxygen) + " (+" + str(Game.oxygen_income) + ", -" + str(Game.oxygen_expense) + ")\n"
+	if show_metal:
+		label_text += "Metal:\n"
+		count_text += str(Game.metal) + " (+" + str(Game.metal_income) + ", -" + str(Game.metal_expense) + ")\n"
+	if show_electricity:
+		label_text += "Electricity:\n"
+		count_text += str(Game.electricity) + " (+" + str(Game.electricity_income) + ", -" + str(Game.electricity_expense) + ")\n"
+	if show_water:
+		label_text += "Science:\n"
+		count_text += str(Game.science) + " (+" + str(Game.science_income) + ", -" + str(Game.science_expense) + ")\n"
+	
+	label_text.strip_edges()
+	count_text.strip_edges()
+	Resource_Label_Text.text = label_text
+	Resource_Amount_Text.text = count_text
