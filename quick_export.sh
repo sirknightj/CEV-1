f809c531b2e191158ba73f5f3f1bd57bc7576e5a
Joshua Lanese <joshua@lanese.org>
2022-05-16 14:08:08 -0700

    Remove rename

--- quick_export.sh
+++ quick_export.sh
@@ -4,4 +4,4 @@
 # (~/web is a symlink to the CSE web home folder)
 tar -czvf cev1.tar.gz export/
 scp cev1.tar.gz froast@attu.cs.washington.edu:~/web
-ssh froast@attu.cs.washington.edu "cd web; rm -rf cev1; tar -xvzf cev1.tar.gz; mv export cev1; mv cev1/cev1.html cev1/index.html; rm cev1.tar.gz; exit"
+ssh froast@attu.cs.washington.edu "cd web; rm -rf cev1; tar -xvzf cev1.tar.gz; mv export cev1; rm cev1.tar.gz; exit"
