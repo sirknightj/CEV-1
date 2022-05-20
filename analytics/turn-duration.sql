f8a2f07b98cac070613aa17154cf018c8dd8f19d
Joshua Lanese <joshua@lanese.org>
2022-05-19 19:42:53 -0700

    Filter category

--- analytics/turn-duration.sql
+++ analytics/turn-duration.sql
@@ -2,14 +2,16 @@
 SELECT
   q1.sessionid,
   q1.qid AS turn,
-  q1.log_q_ts - q2.log_q_ts AS duration 
+  q1.log_q_ts - q2.log_q_ts AS duration
 FROM
   player_quests_log AS q1,
-  player_quests_log AS q2 
+  player_quests_log AS q2
 WHERE
-  q1.qid = q2.qid 
-  AND q1.sessionid = q2.sessionid 
-  AND q1.q_s_id < q2.q_s_id 
+  q1.qid = q2.qid
+  AND q1.sessionid = q2.sessionid
+  AND q1.q_s_id < q2.q_s_id
+  AND q1.cid = 124
+  AND q2.cid = 124
 ORDER BY
   q1.sessionid,
   q1.qid ASC;
